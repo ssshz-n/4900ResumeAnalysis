81f669c42fb04d0baeb5bf7948fde59919a96a92
Niki Lin <nikilin1357@gmail.com>
2026-04-20 11:55:03 -0400

    Updated the sign in sign up functions
    allow user to be added to database for login in

---
+++ javascript/config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    URL: 'https://hvvqfixxijiiurzfwicf.supabase.co',
+    KEY: 'sb_publishable_TL4g7lGTJGmQON8ntpSG2w_SL6cPfom'
+};
--- javascript/index.js
+++ javascript/index.js
@@ -3,6 +3,27 @@ const modal = document.getElementById('auth-modal'); //container
 const modalTitle = document.getElementById('modal-title'); 
 const nameField = document.getElementById('name-field');
 
+let supaBase = null;
+
+//Sets up the connection to supabase using credientals from CONFIG
+function initSupabase(){
+   try{
+      //Checks if CONFIG exists
+      if(typeof CONFIG == 'undefined'){
+         console.error("CONFIG file is missing. File path might be wrong");
+         return false;
+      }
+      //initialize users
+      supaBase = supabase.createClient(CONFIG.URL, CONFIG.KEY);
+      console.log("Supabase connected");
+      return true;
+   }
+   catch(e){
+      console.error("Connection error:", e);
+      return false;
+   }
+}
+
 //function to open signUp
 function signUp(){
    //clears email and password fields when exit/not submitted
@@ -36,17 +57,50 @@ function closeModal(){
 //function for handling form submission (placeholder)
 async function handleAuth(event){
    event.preventDefault();//prevent form from refreshing the page
-
+   
+   //Connecting to Supabase
+   if(!supaBase){
+      const success = initSupabase();
+      if(!success){
+         alert("Database connection failed. Check for errors.");
+         return;
+      }
+   }
+   //gets users information
+   const name = document.getElementById('auth-name').value; //get User name
    const email = document.getElementById('auth-email').value;//get email from user
+   const password = document.getElementById('auth-pass').value;//gets password from sign up
+   const isSignUp = modalTitle.innerText == "Create Account";//determines if its signup or not
 
-   //check if its to create account or is in login -> need to continue to construct a search on database for the user if its in login
-   const type = modalTitle.innerText === "Create Account" ? "signing up" : "logging in";
+   try{
+      //if its Sign up put new users/new user data into the data base
+      if(isSignUp){
+         const {data, error} = await supaBase.from('users').insert([{fullname: name, email: email, password: password}]);
+         if(error){
+            throw error;
+         }
+         alert("Account Successfully Created!");
+      }
+      else{
+         //Login: Checks for users in Supabase users table, matching email and password
+         const{data, error} = await supaBase.from('users').select('*').eq('email', email).eq('password', password).single();
+         if(error|| !data){
+            alert("Invalid Email Or Password.");
+            return;
+         }
+         //Save name in browser memory
+         localStorage.setItem('userName', data.fullname);
+         //Redirect: Go to home page
+         window.location.href = 'home.html';
+      }
+      //close login or sign up when the form is submitted
+      closeModal();
+   }
+   catch(err){
+      console.error("DataBase Error: ", err.message);
+      alert("Error: " + err.message);
+   }
 
-   //we dont have an database yet, so we set an temporary alert as a placeholder for this function
-   alert(`Ready to send ${email} to the MySQL database for ${type}!`);
-
-//close login or sign up when the form is submitted
-   closeModal();
 }
 
 //function for toggling password visibility
